dc315da005714cb2addb09acaac8d6da90f9204e
Gerben272 <gerbenschenk@icloud.com>
2021-05-07 00:05:26 +0200

    first upload of the entire Urk MOD

---
+++ LUA/PolicyReligion.lua
@@ -0,0 +1,26 @@
+-- PolicyReligion
+-- Author: King Augurk B.V., based on GPuzzle's Kingdom of Kent
+-- DateCreated: 4/9/2020 10:58:13 PM
+--------------------------------------------------------------
+--------------------------------------------------------------
+-- Variables
+--------------------------------------------------------------
+local civilizationID = GameInfoTypes["CIVILIZATION_URK"]
+
+--========================================================================================================================
+-- TIME FOR CHANGE
+--========================================================================================================================
+-- CrispyJuice_KoosRabbers_TimeForChange_PolicyReligion
+--------------------------------------------------------------
+function CrispyJuice_KoosRabbers_TimeForChange_PolicyReligion(playerID)
+	local player = Players[playerID]
+	if (player:IsAlive() and player:GetCivilizationType() == civilizationID) then
+		local faithBonus = 12
+		player:ChangeFaith(faithBonus)
+	end
+	print("Dit is een test")
+	print(civilizationID)
+end
+
+GameEvents.PlayerAdoptPolicy.Add(CrispyJuice_KoosRabbers_TimeForChange_PolicyReligion)
+GameEvents.PlayerAdoptPolicyBranch.Add(CrispyJuice_KoosRabbers_TimeForChange_PolicyReligion)
